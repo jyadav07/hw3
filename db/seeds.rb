b906a8458e308a8ab7e8d4a54aa8efd9585b5fbb
Jaya Yadav <jaya.yadav@kellogg.northwestern.edu>
2025-05-20 02:40:47 +0000

    more changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Place.destroy_all
+Entry.destroy_all
+
+# Create places
+mexico = Place.create(name: "Mexico City")
+Place.create(name: "Charleston")
+Place.create(name: "Beijing")
+Place.create(name: "Amsterdam")
+
+# Create entries for Mexico City
+Entry.create(title: "Ate tacos", description: "blah blah", occurred_on: "2022-01-01", place_id: mexico.id)
+Entry.create(title: "Lisa and Jim's wedding", description: "blah blah", occurred_on: "2022-01-01", place_id: mexico.id)
+Entry.create(title: "Ate tacos again", description: "blah blah", occurred_on: "2022-01-01", place_id: mexico.id)
